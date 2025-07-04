186cc53f7939a26f2f3dd882c4d1373161f2bf31
Ziad Khalil <zaskhalil@banquemisr.com>
2025-07-04 16:02:56 +0300

    create vocabulary project

---
+++ Vocabulary/Vocabulary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 04/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vocabulary/Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
