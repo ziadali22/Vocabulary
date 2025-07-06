e910bfe2d9a5d1a809b54aad9d7f8b2653fe9856
Ziad Khalil <zaskhalil@banquemisr.com>
2025-07-06 17:04:51 +0300

    create sucess view

---
+++ Vocabulary/Vocabulary/OnBoarding/OnBoardingFlow/SuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessView.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 06/07/2025.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessView()
+}
