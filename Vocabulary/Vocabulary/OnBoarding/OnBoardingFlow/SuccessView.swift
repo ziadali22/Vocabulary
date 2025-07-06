fbc26a72e9a34e791a554dc803987b06b313f9de
Ziad Khalil <zaskhalil@banquemisr.com>
2025-07-07 00:11:04 +0300

    create success view and integrate lottie animations

--- Vocabulary/Vocabulary/OnBoarding/OnBoardingFlow/SuccessView.swift
+++ Vocabulary/Vocabulary/OnBoarding/OnBoardingFlow/SuccessView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 import Lottie
 
 struct SuccessView: View {
+    let title: String
     let onSelect: () -> Void
     
     @State private var titleVisible = false
     @State private var lottieVisible = false
     @State private var buttonVisible = false
     
+    
     var body: some View {
         VStack(spacing: 40) {
             
-            Text("Customize the app to improve your experience 🎉")
+            Text(title)
                 .font(.title2.bold())
                 .multilineTextAlignment(.center)
                 .foregroundStyle(.white)
@@ -39,17 +41,20 @@ struct SuccessView: View {
                 .opacity(lottieVisible ? 1 : 0)
                 .animation(.spring(response: 1.0, dampingFraction: 0.7).delay(0.4), value: lottieVisible)
             
-            Button("Continue") {
+            Button(action: {
                 onSelect()
+            }) {
+                Text("Continue")
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    .frame(height: 56)
+                    .frame(maxWidth: .infinity)
+                    .background(AppColors.primaryButton)
+                    .cornerRadius(28)
             }
-            .font(.headline)
-            .foregroundColor(.black)
-            .padding()
-            .frame(maxWidth: .infinity, minHeight: 44)
-            .background(AppColors.primaryButton)
-            .cornerRadius(8)
+            .padding(.horizontal)
+            .padding(.top, 32)
             .shadow(color: .black, radius: 1, y: 5)
-            .padding()
             .offset(y: buttonVisible ? 0 : 50)
             .opacity(buttonVisible ? 1 : 0)
             .animation(.spring(response: 0.6, dampingFraction: 0.8).delay(0.6), value: buttonVisible)
@@ -64,5 +69,5 @@ struct SuccessView: View {
 }
 
 #Preview {
-    SuccessView(onSelect: {})
+    SuccessView(title: .init("Success"), onSelect: {})
 }
