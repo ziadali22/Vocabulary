bd4b9e0c3a16f26e052dcb2c70882be1df3cdbc6
Ziad Khalil <zaskhalil@banquemisr.com>
2025-07-07 14:19:47 +0300

    create a design system

---
+++ Vocabulary/Vocabulary/Core/DesignSystem/AppColors.swift
@@ -0,0 +1,20 @@
+//
+//  AppColors.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 07/07/2025.
+//
+
+import SwiftUI
+
+struct AppColors {
+    static let background = Color(hex: "2A324B")
+    static let primaryButton = Color(hex: "C7CCDB")
+    static let primaryText = Color.white
+    static let accent = Color.orange
+    static let border = Color.white.opacity(0.15)
+    static let shadow = Color.black.opacity(0.4)
+    static let pressedShadow = Color.black.opacity(0.2)
+    static let backgroundOptionGradientStart = Color(hex: "#2D2D2D")
+    static let backgroundOptionGradientEnd = Color(hex: "#1A1A1A")
+}
---
+++ Vocabulary/Vocabulary/Core/DesignSystem/AppTypography.swift
@@ -0,0 +1,14 @@
+//
+//  AppTypography.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 07/07/2025.
+//
+
+import SwiftUI
+struct AppTypography {
+    static let title = Font.system(size: 24, weight: .bold)
+    static let body = Font.system(size: 17)
+    static let button = Font.system(size: 16, weight: .semibold)
+    static let headerFont = Font.system(size: 28, weight: .medium)
+}
