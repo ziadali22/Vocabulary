6d53c41ccc742b545d449fdc928c1ed213380954
Ziad Khalil <zaskhalil@banquemisr.com>
2025-07-09 12:36:32 +0300

    create onBoarding container view

---
+++ Vocabulary/Vocabulary/OnBoarding/Presentation/Views/OnboardingContainerView.swift
@@ -0,0 +1,46 @@
+//
+//  OnboardingContainerView.swift
+//  Vocabulary
+//
+//  Created by Ziad Khalil on 09/07/2025.
+//
+
+import SwiftUI
+
+struct OnboardingContainerView: View {
+    @State private var showWalkthrough = false
+    @State private var isOnboardingComplete = false
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingComplete {
+                Text("Main App")
+                    .font(.largeTitle)
+                    .foregroundColor(AppColors.primaryText)
+            } else {
+                ZStack {
+                    IntroView(showWalkThrough: $showWalkthrough)
+                    
+                    OnboardingFlowView(
+                        showWalkThrough: $showWalkthrough,
+                        viewModel: DIContainer.shared.makeOnboardingFlowViewModel {
+                            // Onboarding completed callback
+                            withAnimation {
+                                isOnboardingComplete = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+        .animation(
+            .interactiveSpring(
+                response: 0.85,
+                dampingFraction: 0.85,
+                blendDuration: 0.85
+            ),
+            value: showWalkthrough
+        )
+        .background(AppColors.background)
+    }
+}
